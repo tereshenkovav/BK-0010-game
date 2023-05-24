b31dd5b0c00a60895be08d3c2f24777026328e08
TAV <tereshenkov@mail.ru>
2023-05-24 18:22:39 +0300

    Добавили движение спрайта с полным затираением.

--- src/test_sprite.asm
+++ src/test_sprite.asm
@@ -2,17 +2,42 @@
 
         EMT     14	
 
+	MOV	#10,R0
+	MOV	#320,R1
+START:
+
         MOV	#SPRUNICORN_MIRR,-(SP)   ; Спрайт
-        MOV	#10,-(SP)   ; X
+        MOV	R0,-(SP)   ; X
         MOV	#300,-(SP)  ; Y
         JSR PC, @#DRAWSPRITE
         ADD	#6, SP     ; Восстановить стек на 2*число аргументов
 
         MOV	#SPRUNICORN,-(SP)   ; Спрайт
-        MOV	#320,-(SP)   ; X
+        MOV	R1,-(SP)   ; X
         MOV	#300,-(SP)  ; Y
         JSR PC, @#DRAWSPRITE
         ADD	#6, SP     ; Восстановить стек на 2*число аргументов
+
+	MOV 	#70000,R2
+PAUSE1:	NOP
+	SOB	R2,PAUSE1
+
+        MOV	#SPRUNICORN_MIRR,-(SP)   ; Спрайт
+        MOV	R0,-(SP)   ; X
+        MOV	#300,-(SP)  ; Y
+        JSR PC, @#CLEARSPRITE
+        ADD	#6, SP     ; Восстановить стек на 2*число аргументов
+
+        MOV	#SPRUNICORN,-(SP)   ; Спрайт
+        MOV	R1,-(SP)   ; X
+        MOV	#300,-(SP)  ; Y
+        JSR PC, @#CLEARSPRITE
+        ADD	#6, SP     ; Восстановить стек на 2*число аргументов
+
+	ADD	#4,R0
+	SUB	#4,R1
+
+	JMP 	START
 
 	HALT
 
