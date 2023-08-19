b71cae8a3db4b6dcd0211258a27531140553dd7e
TAV <tereshenkov@mail.ru>
2023-08-19 21:07:28 +0300

    Добавлена генерация новых алмазов случайного типа и положения, убраны начальные алмазы.

--- src/ponydiamonds.asm
+++ src/ponydiamonds.asm
@@ -14,6 +14,11 @@
 	MOV	#0,@#PONYDX
 	MOV	#4,@#PONYDIR
 
+	MOV	@#GENINTERVAL,@#GENCOUNTER
+
+        MOV	#123,R0 ; Начальное значение генерации
+        JSR PC, @#SETRNDSEED
+
         ; Инициализация массива ссылок на спрайты
         MOV	#ARR_SPRITES,R0
         MOV	#SPRDIAMOND1,(R0)+
@@ -37,20 +42,6 @@ CICLE_INIT:
 	MOV	#0,(R0)+
 	MOV	#0,(R0)+
 	SOB 	R1,CICLE_INIT
-
-	; Временные данные алмазов	
-        MOV	#ARR_DIAMONDS,R0
-	MOV	#0,(R0)+
-	MOV	#100,(R0)+
-	MOV	#4,(R0)+
-
-	MOV	#2,(R0)+
-	MOV	#200,(R0)+
-	MOV	#100,(R0)+
-
-	MOV	#5,(R0)+
-	MOV	#300,(R0)+
-	MOV	#40,(R0)+
 
 START:
 	MOV	#3777,@#177706  ; Длительность фрейма (3777 - примерно 10 FPS)
@@ -121,7 +112,7 @@ CICLE_DIAMONDS_CLEAR:
         SUB	@#DIAMONDSPEED,R3
         MOV	R3,-(SP)  ; Y
         MOV	#40,-(SP)  ; DX 
-        MOV	#2,-(SP)  ; DY - размер затираемой области по движению
+        MOV	@#DIAMONDSPEED,-(SP)  ; DY - размер затираемой области по движению
         JSR PC, @#CLEARZONE
         ADD	#10, SP     ; Восстановить стек на 2*число аргументов
         
@@ -216,7 +207,50 @@ SKIP_ARRAY_ELEM2:
         ADD	#4,R0
 	SOB 	R1,CICLE_DIAMONDS_FRAME
 
+	; Генерация новых алмазов
+	DEC	@#GENCOUNTER
+	CMP	@#GENCOUNTER,#0
+	BNE	SKIP_NEW_DIAMOND
+	
+	MOV	@#GENINTERVAL,@#GENCOUNTER
+	
+	MOV	#ARR_DIAMONDS,R0
+        MOV	@#ARR_DIAMONDS_SIZE,R1
+CICLE_DIAMONDS_NEW:
+	CMP	(R0),#-1
+	BNE	SKIP_ARRAY_ELEM4
 
+	MOV	R0,R2 ; save
+
+        MOV	#17,R0 ; Указываем число в интервале от 0 до 15
+        JSR PC, @#GENRNDVALUE
+	CMP	R0,#13
+	BLT	NO_SUB_RND1
+	SUB	#4,R0
+NO_SUB_RND1:
+	MOV	R0,R3
+
+        MOV	#377,R0 ; Указываем число в интервале от 0 до 255
+        JSR PC, @#GENRNDVALUE
+	CMP	R0,#337
+	BLT	NO_SUB_RND2
+	SUB	#40,R0
+NO_SUB_RND2:
+	MOV	R0,R4
+
+	MOV	R2,R0 ; restore
+
+       	MOV	R3,(R0)+
+	MOV	R4,(R0)+
+	MOV	#10,(R0)
+
+        JMP	SKIP_NEW_DIAMOND
+SKIP_ARRAY_ELEM4:
+        ADD	#6,R0
+	SOB 	R1,CICLE_DIAMONDS_NEW
+
+SKIP_NEW_DIAMOND:
+	
 ; ===== блок формирования длины фрейма ======
 TIMERCICLEWAIT:
         BIT	#200,@#177712 ; Тестируем признак прохода через ноль - бит 7
@@ -228,14 +262,17 @@ TIMERCICLEWAIT:
 
 .include "proc_drawsprite.inc"
 .include "proc_keytester.inc"
+.include "proc_genrnd.inc"
 .include "sprites.inc"
 
 PONYX:      .WORD   0
 PONYRENDERX:      .WORD   0
 PONYDX:     .WORD   0
 PONYDIR:    .WORD   0
-DIAMONDSPEED:    .WORD   2
+DIAMONDSPEED:    .WORD   4
 LOWBORDER:    .WORD   300
+GENINTERVAL:	.WORD  14
+GENCOUNTER:    .WORD   0
 ARR_DIAMONDS_SIZE: .WORD 32
 ARR_DIAMONDS: .WORD 0,0,0, 0,0,0, 0,0,0, 0,0,0, 0,0,0, 0,0,0, 0,0,0, 0,0,0
               .WORD 0,0,0, 0,0,0, 0,0,0, 0,0,0, 0,0,0, 0,0,0, 0,0,0, 0,0,0
