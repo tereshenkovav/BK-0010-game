03c57868ab9f95af8f83ced1d6616dd164d964d2
TAV <tereshenkov@mail.ru>
2023-12-21 18:34:25 +0300

    Первичное добавление английского перевода, без коррекции расположения и размеров блоков текста.

--- src/build_by_pdp11asm.bat
+++ src/build_by_pdp11asm.bat
@@ -1,2 +1,7 @@
 UTF8toKOI8R.exe strings-ru.utf8 strings.inc
 pdp11asm ponydiamonds.asm
+copy ponydiamonds.bin ponydiamonds_ru.bin
+
+copy strings-en.utf8 strings.inc
+pdp11asm ponydiamonds.asm
+copy ponydiamonds.bin ponydiamonds_en.bin
--- src/build_by_turbo8.bat
+++ src/build_by_turbo8.bat
@@ -1,2 +1,7 @@
 copy strings-ru.utf8 strings.inc
 BKTurbo8 -i 8 co ponydiamonds.asm
+copy ponydiamonds.bin ponydiamonds_ru.bin
+
+copy strings-en.utf8 strings.inc
+BKTurbo8 -i 8 co ponydiamonds.asm
+copy ponydiamonds.bin ponydiamonds_en.bin
