cb5b51bca796e673ee11e31ded6e9f0e5137d563
TAV <tereshenkov@mail.ru>
2024-10-23 13:46:55 +0300

    Обновление лицензии, дополнение README, отказ от pdp11asm (остался только BKTurbo8).

--- src/build_by_pdp11asm.bat
+++
@@ -1,7 +0,0 @@
-UTF8toKOI8R.exe strings-ru.utf8 strings.inc
-pdp11asm ponydiamonds.asm
-copy ponydiamonds.bin ponydiamonds_ru.bin
-
-copy strings-en.utf8 strings.inc
-pdp11asm ponydiamonds.asm
-copy ponydiamonds.bin ponydiamonds_en.bin
